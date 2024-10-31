13e97091b28ea702efb331b6a70d97e73647505a
Erik Vinícius <erik.vgs@outlook.com>
2024-10-31 14:45:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormularioEmail from './formularioemail';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Formulário de Email</h1>
+            <FormularioEmail />
+        </div>
+    );
+}
+
+export default App;
